132a2e70d6f9669c4f2b210c8df9f0dd0f9cea23
diogo-dgc <diogo@digomaker.cc>
2025-04-10 14:01:56 -0300

    chore: add tailwind css framework

---
+++ README.md
@@ -0,0 +1,34 @@
+# Diogo's personal landing page
+![Static Badge](https://img.shields.io/badge/diogocunha.com-blue?style=for-the-badge&label=Visit&link=https%3A%2F%2Fdiogocunha.com.br%2F) 
+![Static Badge](https://img.shields.io/badge/version-2.0.0-green?style=for-the-badge)
+
+
+## NPM Dependencies
+#### Production (PRD)
+![NPM (prod) Dependency Version](https://img.shields.io/npm/dependency-version/%40tailwindcss%2Fcli/tailwindcss?style=for-the-badge&logo=tailwindcss&link=https%3A%2F%2Ftailwindcss.com%2Fdocs%2Finstallation%2Ftailwind-cli)
+
+#### Development (DEV)
+![NPM dev or peer Dependency Version](https://img.shields.io/npm/dependency-version/prettier-plugin-tailwindcss/dev/prettier?style=for-the-badge&logo=prettier&logoColor=prettier)
+
+
+## Getting started
+### NPM and Node.js
+Be sure you have installed Node.js @^22.8.0 and NPM @^10.8.3
+> [!TIP]
+> Use you prefered terminal to visualize your tools version
+
+```bash
+node -v
+```
+If you don't have Node.js installed, follow [Node.js download instructions](https://nodejs.org/en/download)
+
+```
+npm -v
+```
+If you don't have NPM installed, follow [NPM download instructions](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm)
+
+### Tailwind CSS
++ Follow the 
+[Tailwind CLI instalation](https://tailwindcss.com/docs/installation/tailwind-cli);
++ Use <b>./src/input.css</b> file to custom the theme;
++ Use <b>./src/output.css</b> file to attach in the HTML.
--- src/js/index.js
+++ src/js/index.js
@@ -1,68 +1,65 @@
-const toggleButton = document.getElementById('toggle-btn')
-const sidebar = document.getElementById('sidebar')
-const subMenu = document.getElementById('sub-menu')
+const toggleButton = document.getElementById("toggle-btn");
+const sidebar = document.getElementById("sidebar");
+const subMenu = document.getElementById("sub-menu");
 
-const dropSubMenu = document.querySelectorAll('.dropdown-btn')
-const titlesInMenu = document.querySelectorAll('#menu-title')
+const dropSubMenu = document.querySelectorAll(".dropdown-btn");
+const titlesInMenu = document.querySelectorAll("#menu-title");
 
-
-function toggleSidebar(){
-  sidebar.classList.toggle('close')
-  toggleButton.classList.toggle('rotate')
+function toggleSidebar() {
+  sidebar.classList.toggle("close");
+  toggleButton.classList.toggle("rotate");
 
   toggleTitleMenu();
 }
 
-function toggleTitleMenu(){
-    const isSideBarShrinked = sidebar.classList.contains('close')
-    if(isSideBarShrinked){
-        titlesInMenu.forEach((title) => {
-            console.log(title)
-            title.style.display = 'none'
-        })
-    }else{
-        titlesInMenu.forEach((title) => {
-            console.log(title)
-            title.style.display = ''
-        })
-    }
+function toggleTitleMenu() {
+  const isSideBarShrinked = sidebar.classList.contains("close");
+  if (isSideBarShrinked) {
+    titlesInMenu.forEach((title) => {
+      console.log(title);
+      title.style.display = "none";
+    });
+  } else {
+    titlesInMenu.forEach((title) => {
+      console.log(title);
+      title.style.display = "";
+    });
+  }
 }
 
-function toggleSubMenu(button){
-  
-  preventOpenOtherDropMenu(button.id) //Prevents more than 1 dropdown menu open at same time
-  toggleDropMenuTab(button.classList.contains("show"))
+function toggleSubMenu(button) {
+  preventOpenOtherDropMenu(button.id); //Prevents more than 1 dropdown menu open at same time
+  toggleDropMenuTab(button.classList.contains("show"));
 
-  button.classList.toggle('show')
-  button.classList.toggle('rotate')
+  button.classList.toggle("show");
+  button.classList.toggle("rotate");
 }
 
-function preventOpenOtherDropMenu(itemName){
-  const childQuantities = subMenu.childElementCount
-  dropSubMenu.forEach((item) =>{ 
-    if(item.classList.contains('show')){
-      if(item.id !== itemName){
-        item.classList.remove('show')
-        item.classList.remove('rotate')
+function preventOpenOtherDropMenu(itemName) {
+  const childQuantities = subMenu.childElementCount;
+  dropSubMenu.forEach((item) => {
+    if (item.classList.contains("show")) {
+      if (item.id !== itemName) {
+        item.classList.remove("show");
+        item.classList.remove("rotate");
       }
     }
-  })
-  
-  for(let i = 0; i < childQuantities ; i++){
+  });
+
+  for (let i = 0; i < childQuantities; i++) {
     let subMenuItem = subMenu.children[i];
-    if(subMenuItem.id == itemName){
-      subMenuItem.style.display = ""
-    }else{
-      subMenuItem.style.display = "none"
+    if (subMenuItem.id == itemName) {
+      subMenuItem.style.display = "";
+    } else {
+      subMenuItem.style.display = "none";
     }
   }
 }
 
-function toggleDropMenuTab(itemShow){
-  if(itemShow){
-    subMenu.style.display = "none"
-  }else{
-    subMenu.style.display = "block"
+function toggleDropMenuTab(itemShow) {
+  if (itemShow) {
+    subMenu.style.display = "none";
+  } else {
+    subMenu.style.display = "block";
   }
 }
-
